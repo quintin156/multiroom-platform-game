e3d6a7f6edd9a11d5fcec13a970dc7080c6fbefb
quintin156 <99997788+quintin156@users.noreply.github.com>
2022-10-30 19:02:45 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+
+canvas.width = 64 * 16; //1024
+canvas.height = 64 * 9; //576
+
+c.fillStyle = "white";
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+c.fillStyle = "red";
+c.fillRect(100, 100, 100, 100);
+
+
+function animate() {
+  window.requestAnimationFrame(animate)
+}
+
+animate()
